c053f1ff79b69a952be54fdde902aa488bf644aa
Violet Sirikajornkij <thitirat.sirikajornkij@kellogg.northwestern.edu>
2024-06-04 16:05:47 +0000

    FInal

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,4 @@
 class Entry < ApplicationRecord
+  has_one_attached :uploaded_image
 end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
   
   # So we don't get the "Cannot render console" message in Gitpod
   config.web_console.whiny_requests = false
+
+  # to indicate that we'll use the :local storage option in development.
+  config.active_storage.service = :local
 end
